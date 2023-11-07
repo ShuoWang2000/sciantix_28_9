fba6cdf5c39d972d27a039d982f0231c15d8b65a
giozu <giovanni.zullo@polimi.it>
2023-11-07 10:21:02 +0100

    local/global optimization

--- optimization/parameterOptim/scaling_factor_plot.py
+++ optimization/parameterOptim/scaling_factor_plot.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams['axes.labelsize'] = 20    # Font size for x and y labels
-plt.rcParams['xtick.labelsize'] = 20   # Font size for x-ticks
-plt.rcParams['ytick.labelsize'] = 20  # Font size for y-ticks
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['xtick.labelsize'] = 20
+plt.rcParams['ytick.labelsize'] = 20
 
 def plot_data(file_path1, file_path2):
     data1 = pd.read_csv(file_path1, delimiter='\t')
@@ -22,38 +22,24 @@ def plot_data(file_path1, file_path2):
     n_points = len(time1.to_numpy())
 
     plt.figure(figsize=(10, 6))
-    plt.plot(time1.to_numpy(), diffusivity1.to_numpy()[-1]*np.ones(n_points), label='Helium diffusivity pre-exponential - global offline optimization')
-    plt.scatter(time1.to_numpy(), diffusivity1.to_numpy(), label='Helium diffusivity pre-exponential - stepwise offline optimization')
-    plt.scatter(time2.to_numpy(), diffusivity2.to_numpy(), label='Helium diffusivity pre-exponential - online optimization')
-    
-    # Set labels and title
+    plt.plot(time1.to_numpy(), diffusivity1.to_numpy()[-1]*np.ones(n_points), 'b', label='Helium diffusivity pre-exponential - global offline optimization')
+    plt.plot(time1.to_numpy(), diffusivity1.to_numpy(), '-xb', label='Helium diffusivity pre-exponential - stepwise offline optimization')
+    plt.plot(time2.to_numpy(), diffusivity2.to_numpy(), '-go', label='Helium diffusivity pre-exponential - online optimization')
     plt.xlabel('Time (h)')
     plt.ylabel('Scaling factor')
-    
-    # Show a legend
     plt.legend(loc='best')
-
-    # Show the plot
     plt.show()
     
-    # Create a figure and plot the data
     plt.figure(figsize=(10, 6))
-    plt.plot(time1.to_numpy(), activation_energy1.to_numpy()[-1]*np.ones(n_points), label='Helium diffusivity activation energy - global offline optimization')
-    plt.plot(time1.to_numpy(), activation_energy1.to_numpy(), label='Helium diffusivity activation energy - stepwise offline optimization')
-    plt.scatter(time2.to_numpy(), activation_energy2.to_numpy(), label='Helium diffusivity activation energy - online optimization')
-    
-    # Set labels and title
+    plt.plot(time1.to_numpy(), activation_energy1.to_numpy()[-1]*np.ones(n_points), 'b', label='Helium diffusivity activation energy - global offline optimization')
+    plt.plot(time1.to_numpy(), activation_energy1.to_numpy(), '-xb',label='Helium diffusivity activation energy - stepwise offline optimization')
+    plt.plot(time2.to_numpy(), activation_energy2.to_numpy(), '-go', label='Helium diffusivity activation energy - online optimization')
     plt.xlabel('Time (h)')
     plt.ylabel('Scaling factor')
-    
-    # Show a legend
     plt.legend(loc='best')
-
-    # Show the plot
     plt.show()
 
-# Example usage:
-file_path1 = 'optimization_offline.txt'  # Replace with the actual file path
-file_path2 = 'optimization_online.txt'  # Replace with the actual file path
+file_path1 = 'optimization_offline.txt'
+file_path2 = 'optimization_online.txt'
 
 plot_data(file_path1, file_path2)
--- optimization/parameterOptim/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-with open(f"optimization_{Talip1320.caseName}_{number_of_interval}.txt", 'w') as file:
-    total_rows = len(results_data[0])
-    for index, row in enumerate(results_data):
-        line = "\t".join(row)  # Join columns with a tab separator
-        if index == total_rows - 1:
-            file.write(line)
-        else:
-            file.write(line + "\n")
