a47d3c45d5cc5a484844cd8a7f086c01b74681ab
ShuoWang2000 <wshuo0078@gmail.com>
2023-12-13 12:27:01 +0100

    test

--- optimization/parameterOptim/CalibrationOptimization/main.py
+++ optimization/parameterOptim/CalibrationOptimization/main.py
@@ -23,7 +23,7 @@ def main():
     stds = np.array([info['sigma'] for info in params_info.values()])
 
     
-    time_points = np.linspace(0, max(model.time_exp), 11)
+    time_points = np.linspace(0, max(model.time_exp), 101)
     # Perform Bayesian Calibration
     bc = BayesianCalibration(keys=keys,mean_values=initial_values, stds=stds,sampling_number=51, time_point=time_points)
     bc.bayesian_calibration(model)
