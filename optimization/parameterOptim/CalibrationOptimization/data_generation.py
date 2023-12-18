1708442e91719a66a5513d8d06a1251e3393de22
ShuoWang2000 <wshuo0078@gmail.com>
2023-12-18 17:56:42 +0100

    adjust exploring factor

--- optimization/parameterOptim/CalibrationOptimization/data_generation.py
+++ optimization/parameterOptim/CalibrationOptimization/data_generation.py
@@ -54,7 +54,7 @@ class DataGeneration:
     #     return np.array(new_points)
 
     
-    def _generate_new_points(self,exploration_factor=0.3):
+    def _generate_new_points(self,exploration_factor=0.1):
         """
         Generate new points with a balance between following the original probabilities and exploring the space.
 
