20f31a11bd00b9dc95563471b478b1045283ffaf
ShuoWang2000 <wshuo0078@gmail.com>
2024-01-15 17:31:01 +0100

    more exploration points

--- optimization/parameterOptim/CalibrationOptimization/data_generation.py
+++ optimization/parameterOptim/CalibrationOptimization/data_generation.py
@@ -50,7 +50,7 @@ class DataGeneration:
 
         return exploration_points
 
-    def data_generated(self, exploration_factor=0.4):
+    def data_generated(self, exploration_factor=0.5):
         num_points = self.number_of_new_points
         if self._is_dense():
             exploration_points = int(num_points * exploration_factor)
